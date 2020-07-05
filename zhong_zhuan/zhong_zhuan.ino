f88948c9111dceb3f8254812808bf3850467a8f5
1641420800 <60457240+1641420800@users.noreply.github.com>
2020-07-05 17:03:20 +0800

    emmmmm

--- zhong_zhuan/zhong_zhuan.ino
+++ zhong_zhuan/zhong_zhuan.ino
@@ -121,8 +121,7 @@ void loop()
       p2->p = new ML;
       for (i = 0; i < hc_i; i++) p2->ml[i] = hc[i];
       p2 = p2->p;
-      hc_i = 0;
-      hc[hc_i] = '\n';
+      hc_i = -1;
     }
     hc_i++;
   }
