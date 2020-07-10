0107d63d95d36b575c1bc8066f8a03bde933de8a
1641420800 <60457240+1641420800@users.noreply.github.com>
2020-07-10 23:34:16 +0800

    esp8266

--- esp8266/esp8266.ino
+++ esp8266/esp8266.ino
@@ -4,141 +4,163 @@
 
 #include "aliyun_mqtt.h"
 
-// GPIO 13, D7 on the Node MCU v3
 #define SENSOR_PIN 13
 
 #define WIFI_SSID        "1641420800"//替换自己的WIFI
 #define WIFI_PASSWD      "15612774227"//替换自己的WIFI
 
-#define PRODUCT_KEY      "a1sgsJrxZu5" //替换自己的PRODUCT_KEY
-#define DEVICE_NAME      "znxd_esp" //替换自己的DEVICE_NAME
-#define DEVICE_SECRET    "50f9c2c0bf2628db9f989ed8b2d17474"//替换自己的DEVICE_SECRET
+#define PRODUCT_KEY      "a15FrrRNdXl" //替换自己的PRODUCT_KEY
+#define DEVICE_NAME      "ceshi" //替换自己的DEVICE_NAME
+#define DEVICE_SECRET    "50f4c59aa6025119f20031e989dfbb93"//替换自己的DEVICE_SECRET
 
 #define DEV_VERSION       "S-TH-WIFI-v1.0-20190220"        //固件版本信息
 
-#define ALINK_BODY_FORMAT         "{\"id\":\"123\",\"version\":\"1.0\",\"method\":\"%s\",\"params\":%s}"
+#define ALINK_BODY_FORMAT         "{\"id\":\"111\",\"version\":\"1.0\",\"method\":\"%s\",\"params\":%s}"
 #define ALINK_TOPIC_PROP_POST     "/sys/" PRODUCT_KEY "/" DEVICE_NAME "/thing/event/property/post"
 #define ALINK_TOPIC_PROP_POSTRSP  "/sys/" PRODUCT_KEY "/" DEVICE_NAME "/thing/event/property/post_reply"
 #define ALINK_TOPIC_PROP_SET      "/sys/" PRODUCT_KEY "/" DEVICE_NAME "/thing/service/property/set"
 #define ALINK_METHOD_PROP_POST    "thing.event.property.post"
 #define ALINK_TOPIC_DEV_INFO      "/ota/device/inform/" PRODUCT_KEY "/" DEVICE_NAME ""
-#define ALINK_VERSION_FROMA       "{\"id\": 123,\"params\": {\"version\": \"%s\"}}"
+#define ALINK_VERSION_FROMA       "{\"id\": 111,\"params\": {\"version\": \"%s\"}}"
+
+struct ML {
+  char ml[200] = {'\0'};
+  struct ML *p = NULL;
+};
+struct SHUJU{
+  
+};
+
+int LED_PIN = 2;
+int LED = 0;
+const char fg = ',';                     //分隔符
 unsigned long lastMs = 0;
+unsigned int WAIT_MS = 2000;
+
+struct ML *p1 = new struct ML, *p2 = p1;    //队列出入口
+char ml[200] = {'\0'};                   //通信数据缓存数组
+int ml_i = 0;                          //缓存标识
 
 WiFiClient   espClient;
 PubSubClient mqttClient(espClient);
-
-void init_wifi(const char *ssid, const char *password)
-{
+void init_wifi(const char *ssid, const char *password) {
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.println("WiFi does not connect, try again ...");
+  while (WiFi.status() != WL_CONNECTED)  {
     delay(500);
   }
-
-  Serial.println("Wifi is connected.");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+  Serial.println("Wifi OK\r\n");
 }
-
-void mqtt_callback(char *topic, byte *payload, unsigned int length)
-{
+void mqtt_callback(char *topic, byte *payload, unsigned int length) {
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
   payload[length] = '\0';
   Serial.println((char *)payload);
-
-  if (strstr(topic, ALINK_TOPIC_PROP_SET))
-  {
+  if (strstr(topic, ALINK_TOPIC_PROP_SET))  {
     StaticJsonBuffer<100> jsonBuffer;
     JsonObject &root = jsonBuffer.parseObject(payload);
-    if (!root.success())
-    {
+    if (!root.success())    {
       Serial.println("parseObject() failed");
       return;
     }
   }
 }
-void mqtt_version_post()
-{
+void mqtt_version_post() {
   char param[512];
   char jsonBuf[1024];
-
   //sprintf(param, "{\"MotionAlarmState\":%d}", digitalRead(13));
-  sprintf(param, "{\"id\": 123,\"params\": {\"version\": \"%s\"}}", DEV_VERSION);
+  sprintf(param, "{\"id\": 111,\"params\": {\"version\": \"%s\"}}", DEV_VERSION);
   // sprintf(jsonBuf, ALINK_BODY_FORMAT, ALINK_METHOD_PROP_POST, param);
   Serial.println(param);
   mqttClient.publish(ALINK_TOPIC_DEV_INFO, param);
 }
-void mqtt_check_connect()
-{
-  while (!mqttClient.connected())//mqttδ����
-  {
-    while (connect_aliyun_mqtt(mqttClient, PRODUCT_KEY, DEVICE_NAME, DEVICE_SECRET))
-    {
+void mqtt_check_connect() {
+  while (!mqttClient.connected()) {
+    while (connect_aliyun_mqtt(mqttClient, PRODUCT_KEY, DEVICE_NAME, DEVICE_SECRET))    {
       Serial.println("MQTT connect succeed!");
       //client.subscribe(ALINK_TOPIC_PROP_POSTRSP);
       mqttClient.subscribe(ALINK_TOPIC_PROP_SET);
-
       Serial.println("subscribe done");
       mqtt_version_post();
     }
   }
-
 }
-
-void mqtt_interval_post()
-{
+void mqtt_interval_post() {
   char param[512];
   char jsonBuf[1024];
-
-  //sprintf(param, "{\"MotionAlarmState\":%d}", digitalRead(13));
-  sprintf(param, "{\"CurrentHumidity\":%d,\"CurrentTemperature\":12,\"GeoLocation\":{\"CoordinateSystem\":2,\"Latitude\":2,\"Longitude\":123,\"Altitude\":1}}", digitalRead(13));
+  sprintf(param, "{\"guangqiang\":%d,\"huoyan\":%d,\"kongqishidu\":%d,\"kongqizhiliang\":%d,\"turangshidu\":%d,\"wendu\":%d,\"yudi\":%d}", 77, 1, 33, 33, 22, 11, 11);
   sprintf(jsonBuf, ALINK_BODY_FORMAT, ALINK_METHOD_PROP_POST, param);
   Serial.println(jsonBuf);
   mqttClient.publish(ALINK_TOPIC_PROP_POST, jsonBuf);
 }
-
-
-void setup()
+int cin(char* p, int x) //字符串转整数-p中第x个数字,数字间用","(分隔符)隔开
 {
-
-  pinMode(SENSOR_PIN, INPUT);
-  /* initialize serial for debugging */
+  int s = 0, fh = 1, i;
+  for (i = 0; i < 200; i++) {
+    if (!x) break;
+    if (p[i] == fg) x--;
+  }
+  if (i == 199) return -1;
+  p += i;
+  if (*p == '-') {
+    fh = -1;
+    p++;
+  }
+  while (*p >= '0' && *p <= '9') {
+    s *= 10;
+    s += *p - '0';
+    p++;
+  }
+  return s * fh;
+}
+int mlcl(char* s, char* ml)           //命令处理-判断s的开头是否为ml 是-将s中ml删掉
+{
+  char* p1 = s, * p2 = ml;
+  while (*p1 == *p2) {
+    p1++, p2++;
+    if (*p1 == '\0') break;
+  }
+  if (*p2 != '\0') return 0;
+  else if (*p1 == '\0') return 1;
+  else for (int i = 0; p1[i - 1] != '\0'; i++) s[i] = p1[i];
+  return 2;
+}
+void setup() {
+  pinMode(2, OUTPUT);
   Serial.begin(115200);
-
-  Serial.println("Demo Start");
-
   init_wifi(WIFI_SSID, WIFI_PASSWD);
-
   mqttClient.setCallback(mqtt_callback);
 }
+void loop() {
+  int i;
+  struct ML *p = NULL;
+  while (Serial.available()) {                          //处理串口
+    ml[ml_i] = char(Serial.read());
+    if (ml[ml_i] == '$' && ml_i != 0) {
+      p2->p = new ML;
+      for (i = 0; i < ml_i; i++) p2->ml[i] = ml[i];
+      p2 = p2->p;
+      ml_i = 0;
+      ml[ml_i] = '$';
+    }
+    ml_i++;
+  }
+  while (p1->p) {
+    if (mlcl(p1->ml, "gz")) {
 
-// the loop function runs over and over again forever
-void loop()
-{
-  if (millis() - lastMs >= 20000)
-  {
+    }
+
+    p = p1;
+    p1 = p1->p;
+    delete p;
+  }
+  if (millis() - lastMs >= 20000)  {
     lastMs = millis();
     mqtt_check_connect();
-    /* Post */
-    mqtt_interval_post();
+    mqtt_interval_post();     //上交数据
+    digitalWrite(LED_PIN, LED = !LED);
   }
-
   mqttClient.loop();
-
-  unsigned int WAIT_MS = 2000;
-  if (digitalRead(SENSOR_PIN) == HIGH)
-  {
-    Serial.println("Motion detected!");
-  }
-  else
-  {
-    Serial.println("Motion absent!");
-  }
   delay(WAIT_MS); // ms
-  Serial.println(millis() / WAIT_MS);
 }
