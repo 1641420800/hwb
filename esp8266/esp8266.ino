cb3c5c8ca5bff1428d7c6b2a44ecc8406267042e
1641420800 <60457240+1641420800@users.noreply.github.com>
2020-07-12 23:18:38 +0800

    esp-01 基本完成

--- esp8266/esp8266.ino
+++ esp8266/esp8266.ino
@@ -26,14 +26,14 @@ struct ML {
   struct ML *p = NULL;
 };
 struct SHUJU {
-  int   gm      = 0;
-  int   hy      = 0;
-  float mq      = 0;
-  float dth[2]  = {0};
-  float yd      = 0;
-  float tr      = 0;
+  int   gz      = 0;    //光照强度
+  int   hy      = 0;    //火焰
+  int   mq      = 0;    //空气质量
+  float dth[2]  = {0};  //温湿度
+  int   yd      = 0;    //雨滴
+  int   tr      = 0;    //土壤
 };
-struct BIAO{
+struct BIAO {
   int bh = 0;
   struct SHUJU *p = NULL;
 };
@@ -45,21 +45,25 @@ unsigned long lastMs = 0;
 unsigned int WAIT_MS = 2000;
 
 struct ML *p1 = new struct ML, *p2 = p1;    //队列出入口
-struct BIAO biao[100];
-struct SHUJU pjsj;
+struct BIAO biao[100];                      //表
+int yicun = 0;                              //已存
+struct SHUJU pjsj;                          //平均数据
 char ml[200] = {'\0'};                      //通信数据缓存数组
 int ml_i = 0;                               //缓存标识
 WiFiClient   espClient;
 PubSubClient mqttClient(espClient);
 //=======================================================================================================
 int cch(char* s, int j, char* p2, int n, char a = ',');
+int cin(char* p, int x);
+int mlcl(char* s, char* ml);
+void dch(double d, int bl, char *p);
 void init_wifi(const char *ssid, const char *password);
 void mqtt_callback(char *topic, byte *payload, unsigned int length);
 void mqtt_version_post();
 void mqtt_check_connect();
 void mqtt_interval_post();
-int cin(char* p, int x);
-int mlcl(char* s, char* ml);
+struct SHUJU* bcdz(int bh);
+void pingjun();
 
 //=======================================================================================================
 void setup() {
@@ -71,6 +75,7 @@ void setup() {
 void loop() {
   int i;
   struct ML *p = NULL;
+  struct SHUJU * sj;
   char lss[200] = { 0 };
   while (Serial.available()) {                          //处理串口
     ml[ml_i] = char(Serial.read());
@@ -84,45 +89,46 @@ void loop() {
     ml_i++;
   }
   while (p1->p) {
-/*    if (mlcl(p1->ml, "gm")) {
-      shuju.bianhao = cin(p1->ml, 0);
-      cch(p1->ml, 200, lss, 1);
+    if (mlcl(p1->ml, "gm")) {         //光照
+      sj = bcdz( cin(p1->ml, 0));
+      sj -> gz = cin(p1->ml, 1);
     }
-    if (mlcl(p1->ml, "hy")) {
-      shuju.bianhao = cin(p1->ml, 0);
-      cch(p1->ml, 200, lss, 1);
+    if (mlcl(p1->ml, "hy")) {         //火焰
+      sj = bcdz( cin(p1->ml, 0));
+      sj -> hy = cin(p1->ml, 1);
     }
-    if (mlcl(p1->ml, "mq")) {
-      shuju.bianhao = cin(p1->ml, 0);
-      cch(p1->ml, 200, lss, 1);
+    if (mlcl(p1->ml, "mq")) {         //空气质量
+      sj = bcdz( cin(p1->ml, 0));
+      sj -> mq = cin(p1->ml, 1);
     }
-    if (mlcl(p1->ml, "dth")) {
-      shuju.bianhao = cin(p1->ml, 0);
-      cch(p1->ml, 200, lss, 1);
-      cch(p1->ml, 200, lss, 2);
+    if (mlcl(p1->ml, "dth")) {        //温湿度
+      sj = bcdz( cin(p1->ml, 0));
+      sj -> dth[0] = cdo(p1->ml, 1);
+      sj -> dth[1] = cdo(p1->ml, 2);
     }
-    if (mlcl(p1->ml, "yd")) {
-      shuju.bianhao = cin(p1->ml, 0);
-      cch(p1->ml, 200, lss, 1);
+    if (mlcl(p1->ml, "yd")) {         //雨滴
+      sj = bcdz( cin(p1->ml, 0));
+      sj -> yd = cin(p1->ml, 1);
     }
-    if (mlcl(p1->ml, "tr")) {
-      shuju.bianhao = cin(p1->ml, 0);
-      cch(p1->ml, 200, lss, 1);
+    if (mlcl(p1->ml, "tr")) {         //土壤
+      sj = bcdz( cin(p1->ml, 0));
+      sj -> tr = cin(p1->ml, 1);
     }
 
-*/
+
     p = p1;
     p1 = p1->p;
     delete p;
   }
   if (millis() - lastMs >= 20000)  {
     lastMs = millis();
+    pingjun();
     mqtt_check_connect();
     mqtt_interval_post();     //上交数据
     digitalWrite(LED_PIN, LED = !LED);
+    mqttClient.loop();
   }
-  mqttClient.loop();
-  delay(WAIT_MS); // ms
+  //delay(WAIT_MS); // ms
 }
 //======================================================================================================= WIFI连接
 void init_wifi(const char *ssid, const char *password) {
@@ -170,7 +176,11 @@ void mqtt_check_connect() {
 void mqtt_interval_post() {
   char param[512];
   char jsonBuf[1024];
-  sprintf(param, "{\"guangqiang\":%d,\"huoyan\":%d,\"kongqishidu\":%d,\"kongqizhiliang\":%d,\"turangshidu\":%d,\"wendu\":%d,\"yudi\":%d}", 99, 2, 55, 44, 222, 111, 141);
+  char s1[20], s2[20];
+  dch((double)pjsj.dth[0], 2, s1);
+  dch((double)pjsj.dth[1], 2, s2);
+  sprintf(param, "{\"guangqiang\":%d,\"huoyan\":%d,\"kongqishidu\":%s,\"kongqizhiliang\":%d,\"turangshidu\":%d,\"wendu\":%s,\"yudi\":%d}"
+          , pjsj.gz, pjsj.hy, s1 , pjsj.mq, pjsj.tr, s2, pjsj.yd);
   sprintf(jsonBuf, ALINK_BODY_FORMAT, ALINK_METHOD_PROP_POST, param);
   mqttClient.publish(ALINK_TOPIC_PROP_POST, jsonBuf);
 }
@@ -229,17 +239,80 @@ int cch(char* s, int j, char* p2, int n, char a) {
   return -1;
 }
 double cdo(char* p, int n) {
-    int a = cin(p, n), i;
-    for (i = 0; i < 200; i++) {
-        if (!n) break;
-        if (p[i] == fg) n--;
-    }
-    if (i == 199) return -1;
-    p += i;
-    while (*p != '.') p++;
+  int a = cin(p, n), i;
+  for (i = 0; i < 200; i++) {
+    if (!n) break;
+    if (p[i] == fg) n--;
+  }
+  if (i == 199) return -1;
+  p += i;
+  while (*p != '.') p++;
+  p++;
+  double b = (double)cin(p, n);
+  while (b > 1) b /= 10;
+  b = a + b;
+  return b;
+}
+void dch(double d, int bl, char *p) {
+  int ws = 0, ls = 0;
+  if (d < 1 && d > -1) ls = 1;
+  while (d > 1 || d < -1) {
+    d /= 10;
+    ws++;
+  }
+  if (d < 0) {
+    *p = '-';
     p++;
-    double b = (double)cin(p, n);
-    while (b > 1) b /= 10;
-    b = a + b;
-    return b;
+    d *= -1;
+  }
+  if (ls) {
+    *p = '0';
+    p++;
+  }
+  while (d != 0) {
+    if (!ws) {
+      *p = '.';
+      p++;
+    }
+    if (ws == -1 * bl) break;
+    ls = (int)(d * 10);
+    d = d * 10 - ls;
+    *p = '0' + ls % 10;
+    p++;
+    ws--;
+  }
+  *p = '\0';
+}
+//======================================================================================================= 数据储存支持部分
+struct SHUJU* bcdz(int bh) {
+  int i;
+  struct SHUJU* p = NULL;
+  for (i = 0; i < yicun; i++) {
+    if (biao[i].bh == bh) return biao[i].p;
+  }
+  if (yicun < 100) {
+    biao[yicun].bh = bh;
+    p = new struct SHUJU;
+    yicun++;
+    return p;
+  }
+  return NULL;
+}
+void pingjun() {
+  pjsj.hy = pjsj.gz = pjsj.mq = pjsj.yd = pjsj.tr = 0;
+  pjsj.dth[0] = pjsj.dth[1] = 0;
+  for (int i; i < yicun; i++) {
+    pjsj.hy += biao[i].p->hy;
+    pjsj.gz += biao[i].p->gz;
+    pjsj.mq += biao[i].p->mq;
+    pjsj.tr += biao[i].p->tr;
+    pjsj.dth[0] += biao[i].p->dth[0];
+    pjsj.dth[1] += biao[i].p->dth[1];
+  }
+  if (pjsj.hy) pjsj.hy = 1;
+  pjsj.gz /= yicun;
+  pjsj.mq /= yicun;
+  pjsj.tr /= yicun;
+  pjsj.dth[0] /= yicun;
+  pjsj.dth[1] /= yicun;
 }
